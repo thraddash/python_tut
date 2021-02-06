786f78bfc53aece397a0408cd4b4fc4ee26cc818
thraddash <theartist@gmail.com>
2021-02-05 18:42:35 -0800

    web scrab test

---
+++ 23_bonus_python_projects/1_web_scrapping/demo.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+## News title, link scraping
+
+from webscrap import wlog
+from webscrap import wscrap
+
+# Define log file location
+wlog.set_custom_log_info('html/error.log')
+
+'''
+# Testing log file reporting
+try:
+    raise Exception
+except Exception as e:
+    wlog.report(str(e))
+'''
+
+news_scrap = wscrap.NewsScraper(wscrap.url_aj, wlog)
+
+## UNCOMMENT THE FOLLOWING 2 LINES OF CODE TO GET SITE'S LATEST DATA
+## OTHERWISE THIS PROGRAM PARSE DATA FROM DISK FILE RETRIEVED PREVIOUSLY
+
+news_scrap.retrieve_webpage()
+news_scrap.write_webpage_as_html()
+
+news_scrap.read_webpage_from_html()
+news_scrap.convert_data_to_bs4()
+news_scrap.print_beautiful_soup()
+news_scrap.parse_soup_to_simple_html()
---
+++ 23_bonus_python_projects/1_web_scrapping/webscrap/wlog.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import logging
+
+def set_custom_log_info(filename):
+    logging.basicConfig(filename=filename, level=logging.INFO)
+
+def report(e:Exception):
+    logging.exception(str(e))
